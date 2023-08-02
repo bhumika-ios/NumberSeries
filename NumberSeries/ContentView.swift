b061e450706a037ff1a4901ef0e1b7e27b1c68b2
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-08-02 14:13:54 +0530

    add number and number show in box

--- NumberSeries/ContentView.swift
+++ NumberSeries/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var numberString = ""
+    @State private var maximumNumber = 0
+    @State private var currentIndex = 0
+    
+    private var numberRange: ClosedRange<Int> {
+        1...max(maximumNumber, 1)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Enter a number greater than 0", text: $numberString, onCommit: {
+                if let number = Int(numberString), number > 0 {
+                    maximumNumber = number
+                    currentIndex = 1 // Reset the current index when the maximum number changes.
+                }
+            })
+            .keyboardType(.numberPad)
+            
+            HStack {
+                Button("Prev", action: prevButtonTapped)
+                Spacer()
+                Button("Next", action: nextButtonTapped)
+            }
+            .padding()
+            ScrollView(.horizontal){
+                HStack {
+                   
+                    ForEach(numberRange, id: \.self) { num in
+                        
+                        BoxView(number: num, isActive: num == currentIndex)
+                    }
+                }
+            }
         }
         .padding()
+    }
+    
+    private func nextButtonTapped() {
+        if maximumNumber > 1 {
+            currentIndex = (currentIndex % maximumNumber) + 1
+        }
+    }
+    
+    private func prevButtonTapped() {
+        if maximumNumber > 1 {
+            currentIndex = (currentIndex - 2 + maximumNumber) % maximumNumber + 1
+        }
+    }
+}
+
+struct BoxView: View {
+    let number: Int
+    let isActive: Bool
+    
+    var body: some View {
+        Text("\(number)")
+            .foregroundColor(isActive ? .white : .black)
+            .frame(width: 50, height: 50)
+            .background(isActive ? Color.blue : Color.gray)
+            .cornerRadius(8)
+            .padding(5)
     }
 }
 
