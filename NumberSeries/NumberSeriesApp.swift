f0b93820d80f9933133a2a68632cf937f9c7fe58
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-08-02 12:47:30 +0530

    Initial Commit

---
+++ NumberSeries/NumberSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberSeriesApp.swift
+//  NumberSeries
+//
+//  Created by Bhumika Patel on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NumberSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
